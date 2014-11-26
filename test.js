534ab7b67329a0b18e993230e53cc31c55f1aa1b
donvoo <donvoowright@aol.com>
2014-11-25 18:29:58 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 API.on(API.chatLog("donvoo's Skip Calculator",1));
 API.on(API.CHAT_COMMAND, test);
-var testTest = '100';
-var laLa = "hello world";
+var bug = '100';
 function test(data) {
     var command = data;
     if (command == "/meh"){
