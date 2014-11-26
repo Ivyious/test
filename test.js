6f513c9cb757675852cf01f814bf1b5a17371bf6
donvoo <donvoowright@aol.com>
2014-11-25 18:59:58 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
 API.on(API.chatLog("donvoo's Skip Calculator",1));
 API.on(API.CHAT_COMMAND, test);
 var bug = '100';
+var bu = bug / .075;
 function test(data) {
     var command = data;
     if (command == "/meh"){
     bug = prompt("Please Input How Many People Are In The Room");
-    API.chatLog(bug);
+    API.chatLog("Mehs To Skip:");
+    API.chatLog(bu);
     }
 }
 
