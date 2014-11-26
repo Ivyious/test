7b19df8d1b5d239dc1e409bfa370f08390b9a869
donvoo <donvoowright@aol.com>
2014-11-25 19:40:35 -0700

    Update test.js

--- test.js
+++ test.js
@@ -4,13 +4,14 @@ var bug = '100';
 var bu = '0';
 var b = API.getUsers().length;
 var c = b * .075;
+var d = Math.round(c)
 function test(data) {
     var command = data;
     if (command == "/meh"){
     //bug = prompt("Please Input How Many People Are In The Room");
     bu = bug * .075
     API.chatLog("Mehs To Skip",1);
-    API.chatLog(JSON.stringify(c),1);
+    API.chatLog(JSON.stringify(d),1);
     }
 }
 
