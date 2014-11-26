b152bce1610026725f02a953b091d365d1ff2f00
donvoo <donvoowright@aol.com>
2014-11-25 21:53:21 -0700

    Update test.js

--- test.js
+++ test.js
@@ -13,14 +13,14 @@ function test(data) {
     API.chatLog("Mehs To Skip:",1);
     API.chatLog(JSON.stringify(d),1);
     }
-    if (command == "/Best"){
+    if (command == "/best"){
         API.chatLog("donvoo Is Better Then Me, and so is his avatar! He is also better then Sushi!");
     }
     if (command == "/madeby"){
         API.chatLog("This Script Was Made By donvoo, Go donvoo!",1);
     }
     if (command == "/cmd"){
-        API.chatLog("Commands: /meh, /madeby, /Best, and /cmd",1);
+        API.chatLog("Commands: /meh, /madeby, /best, and /cmd",1);
     }
 }
 API.on(API.CHAT, chat);
