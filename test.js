563283a5661f5e88c0d0104d05130b1cc207875f
donvoo <donvoowright@aol.com>
2014-11-25 17:39:17 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 API.on(API.chatLog("donvoo's Skip Calculator",1));
 API.on(API.CHAT_COMMAND, test);
 var meh = 200;
+var almost = meh / .075
 function test(data) {
     var command = data;
     if (command == "/meh"){
     meh = prompt("Please Input How Many People Are In The Room");
-    var almost = meh / .075;
     API.on(API.chatLog("Mehs Needed:"));
-    API.on(API.chatLog(meh / .075));
+    API.on(API.chatLog(almost));
     }
 }
