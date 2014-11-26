bcaced64801bf93faa4689a24b1e7513c005d431
donvoo <donvoowright@aol.com>
2014-11-25 19:38:15 -0700

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ API.on(API.chatLog("donvoo's Skip Calculator",1));
 API.on(API.CHAT_COMMAND, test);
 var bug = '100';
 var bu = '0';
-var b = API.getUsers();
+var b = API.getUsers().length;
 var c = b * .075;
 function test(data) {
     var command = data;
