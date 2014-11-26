42016acb76c157d8047bc7e5cba226e24a06c359
donvoo <donvoowright@aol.com>
2014-11-25 18:15:35 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 API.on(API.chatLog("donvoo's Skip Calculator",1));
 API.on(API.CHAT_COMMAND, test);
-var testtest = 100
+var testtest = 100;
 var lala = "hello world"
 function test(data) {
     var command = data;
