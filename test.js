b9dadb2e2b722a58b19452d7bd52c66a89b50232
donvoo <donvoowright@aol.com>
2014-11-25 18:32:22 -0700

    Update test.js

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ function test(data) {
     var command = data;
     if (command == "/meh"){
     bug = prompt("Please Input How Many People Are In The Room");
-    API.chatLog("largo");
+    API.chatLog("larg");
     }
 }
 
